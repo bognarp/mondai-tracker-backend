00eb270ed520684db0cb05bd7bdf936afe904c8f
bognarp <21261487+bognarp@users.noreply.github.com>
2022-02-02 20:31:35 +0100

    feat: setup config files
    Create eslint, prettier rules. config for dotenv.

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const config = require('./utils/config');
 const cors = require('cors');
-const app = express();
+const morgan = require('morgan');
 const usersRouter = require('./routes/api/users');
 const projectsRouter = require('./routes/api/projects');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB succesfully'))
+  .catch((err) => console.log(err));
+
+const app = express();
 
 app.disable('x-powered-by');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.use('/api/users', usersRouter);
 app.use('/api/projects', projectsRouter);
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server is running on port ${config.PORT}`);
 });

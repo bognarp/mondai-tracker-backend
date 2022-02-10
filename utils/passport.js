68b422a1d5c4e3823480d93bfdf4d19f750ff0c6
bognarp <21261487+bognarp@users.noreply.github.com>
2022-02-10 22:41:59 +0100

    fix: temporarily add populate to auth req

--- utils/passport.js
+++ utils/passport.js
@@ -10,9 +10,15 @@ options.secretOrKey = SECRET;
 module.exports = (passport) => {
   passport.use(
     new JwtStrategy(options, (jwt_payload, done) => {
+      // FIXME: This creates way more queries than needed:
+      // Every protected req-res cycle has to query Users and
+      // then Projects.
       User.findById(jwt_payload.id)
+        .populate('ownProjects', { title: 1 })
+        .populate('memberProjects', { title: 1 })
         .then((user) => {
           if (user) {
+            console.log('passport query callback...');
             return done(null, user);
           }
 

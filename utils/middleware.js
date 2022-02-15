314a1af1db7c27c60cf201c4cd0f94ca0c103e78
bognarp <21261487+bognarp@users.noreply.github.com>
2022-02-15 23:17:29 +0100

    refactor: rename parameters

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,17 +1,17 @@
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
 };
 
-const errorHandler = (error, request, response, next) => {
+const errorHandler = (error, req, res, next) => {
   console.error('ERROR NAME: ', error.name);
   console.error('ERROR MESSAGE: ', error.message);
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return res.status(400).send({ error: 'malformatted id' });
   }
 
   if (error.name === 'MongooseError') {
-    return response.status(500).send({ error: 'server error' });
+    return res.status(500).send({ error: 'server error' });
   }
 
   next(error);
